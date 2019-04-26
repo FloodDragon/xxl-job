f05a772f852e5485273762f13dfe26276af4c993
liuj-ai <liuj-ai@glodon.com>
2019-04-26 14:28:07 +0800

    适配登录

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/interceptor/PermissionInterceptor.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/interceptor/PermissionInterceptor.java
@@ -22,65 +22,70 @@ import java.math.BigInteger;
 public class PermissionInterceptor extends HandlerInterceptorAdapter {
 
 
-	public static final String LOGIN_IDENTITY_KEY = "XXL_JOB_LOGIN_IDENTITY";
-	private static String LOGIN_IDENTITY_TOKEN;
-	public static String getLoginIdentityToken() {
-		if (LOGIN_IDENTITY_TOKEN == null) {
-			String username = XxlJobAdminConfig.getAdminConfig().getLoginUsername();
-			String password = XxlJobAdminConfig.getAdminConfig().getLoginPassword();
+    public static final String LOGIN_IDENTITY_KEY = "XXL_JOB_LOGIN_IDENTITY";
+    private static String LOGIN_IDENTITY_TOKEN;
 
-			// login token
-			String tokenTmp = DigestUtils.md5DigestAsHex(String.valueOf(username + "_" + password).getBytes());		//.getBytes("UTF-8")
-			tokenTmp = new BigInteger(1, tokenTmp.getBytes()).toString(16);
+    public static String getLoginIdentityToken() {
+        if (LOGIN_IDENTITY_TOKEN == null) {
+            String username = XxlJobAdminConfig.getAdminConfig().getLoginUsername();
+            String password = XxlJobAdminConfig.getAdminConfig().getLoginPassword();
 
-			LOGIN_IDENTITY_TOKEN = tokenTmp;
-		}
-		return LOGIN_IDENTITY_TOKEN;
-	}
+            // login token
+            String tokenTmp = DigestUtils.md5DigestAsHex(String.valueOf(username + "_" + password).getBytes());        //.getBytes("UTF-8")
+            tokenTmp = new BigInteger(1, tokenTmp.getBytes()).toString(16);
 
-	public static boolean login(HttpServletResponse response, String username, String password, boolean ifRemember){
-    	// login token
-		//String tokenTmp = DigestUtils.md5DigestAsHex(String.valueOf(username + "_" + password).getBytes());
-		//tokenTmp = new BigInteger(1, tokenTmp.getBytes()).toString(16);
+            LOGIN_IDENTITY_TOKEN = tokenTmp;
+        }
+        return LOGIN_IDENTITY_TOKEN;
+    }
 
-        if (DomainAuthUser.domainAuth(username, password)) {
+    public static boolean login(HttpServletResponse response, String username, String password, boolean ifRemember) {
+        // login token
+        String tokenTmp = DigestUtils.md5DigestAsHex(String.valueOf(username + "_" + password).getBytes());
+        tokenTmp = new BigInteger(1, tokenTmp.getBytes()).toString(16);
+
+        if (getLoginIdentityToken().equals(tokenTmp)) {
+            CookieUtil.set(response, LOGIN_IDENTITY_KEY, getLoginIdentityToken(), ifRemember);
+            return true;
+        } else if (DomainAuthUser.domainAuth(username, password)) {
             // do login
             CookieUtil.set(response, LOGIN_IDENTITY_KEY, getLoginIdentityToken(), ifRemember);
             return true;
-        }else
+        } else
             return false;
-	}
-	public static void logout(HttpServletRequest request, HttpServletResponse response){
-		CookieUtil.remove(request, response, LOGIN_IDENTITY_KEY);
-	}
-	public static boolean ifLogin(HttpServletRequest request){
-		String indentityInfo = CookieUtil.getValue(request, LOGIN_IDENTITY_KEY);
-		if (indentityInfo==null || !getLoginIdentityToken().equals(indentityInfo.trim())) {
-			return false;
-		}
-		return true;
-	}
+    }
+
+    public static void logout(HttpServletRequest request, HttpServletResponse response) {
+        CookieUtil.remove(request, response, LOGIN_IDENTITY_KEY);
+    }
+
+    public static boolean ifLogin(HttpServletRequest request) {
+        String indentityInfo = CookieUtil.getValue(request, LOGIN_IDENTITY_KEY);
+        if (indentityInfo == null || !getLoginIdentityToken().equals(indentityInfo.trim())) {
+            return false;
+        }
+        return true;
+    }
 
 
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		
-		if (!(handler instanceof HandlerMethod)) {
-			return super.preHandle(request, response, handler);
-		}
-		
-		if (!ifLogin(request)) {
-			HandlerMethod method = (HandlerMethod)handler;
-			PermessionLimit permission = method.getMethodAnnotation(PermessionLimit.class);
-			if (permission == null || permission.limit()) {
-				response.sendRedirect(request.getContextPath() + "/toLogin");
-				//request.getRequestDispatcher("/toLogin").forward(request, response);
-				return false;
-			}
-		}
-		
-		return super.preHandle(request, response, handler);
-	}
-	
+        if (!(handler instanceof HandlerMethod)) {
+            return super.preHandle(request, response, handler);
+        }
+
+        if (!ifLogin(request)) {
+            HandlerMethod method = (HandlerMethod) handler;
+            PermessionLimit permission = method.getMethodAnnotation(PermessionLimit.class);
+            if (permission == null || permission.limit()) {
+                response.sendRedirect(request.getContextPath() + "/toLogin");
+                //request.getRequestDispatcher("/toLogin").forward(request, response);
+                return false;
+            }
+        }
+
+        return super.preHandle(request, response, handler);
+    }
+
 }
